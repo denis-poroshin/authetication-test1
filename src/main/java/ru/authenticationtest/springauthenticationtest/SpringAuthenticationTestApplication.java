72f704389431a4c25f5b1e65469fb96b003d7627
PC <git@github.com:denis-poroshin/store1.git>
2024-09-09 18:37:57 +0300

    Начало

---
+++ src/main/java/ru/authenticationtest/springauthenticationtest/SpringAuthenticationTestApplication.java
@@ -0,0 +1,13 @@
+package ru.authenticationtest.springauthenticationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthenticationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthenticationTestApplication.class, args);
+	}
+
+}
